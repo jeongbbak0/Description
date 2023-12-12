d7879fe778040eae885b4ab0c788cc17c38968c0
이정민 <135570599+jeongbbak0@users.noreply.github.com>
2023-12-12 19:36:22 +0900

    Create README.py

---
+++ README.py
@@ -0,0 +1,12 @@
+print("========================")
+print("= 구구단 출력 프로그램 =")
+print("= by 홍길동(건축학과) =")
+print("========================")
+
+while True:
+    dan = int(input("몇 단을 출력할까요? "))
+    for i in range(1, 10):
+        print(f"{dan} x {i} = {dan*i}")
+    answer = input("계속하려면 y를 입력하시고 멈추려면 n 입력하세요. ")
+    if answer.lower() == 'n':
+        break
